bdc4efe8e2b1034c0ddc09e9ccf9510d6e52c737
hahs <alexandre_2420@outlook.com>
2022-07-21 08:40:26 -0500

    [added]: sidebar component

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//es necesario xq este modulo va utilizar routerLink
+import { RouterModule } from '@angular/router';
+//components
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [SidebarComponent],
 })
 export class SearchModule {}
